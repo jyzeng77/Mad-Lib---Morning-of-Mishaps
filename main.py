b386bf8bc92f57f479b3051b06ebe240a68e2c2f
Jerry <77946782+jyzeng77@users.noreply.github.com>
2023-02-17 23:57:57 +0000

    Made additions, cleaned up code, put comments in

--- main.py
+++ main.py
@@ -3,119 +3,187 @@
 #
 # @author Jerry Zeng
 # @course ICS3UC
-# @date YYYY/MM/DD - 2023/02/17
+# @date 2023/02/17 - LastModified
 ##
 
 ###*** DEFINITIONS ***#
-import time
-
-# Prints text using typewriter effect.
-def typed(string, speed=0.1, ending='\n'):
-  for c in string:
-    print(c, end='', flush=True)
-    time.sleep(speed)
-  print(end=ending)
 
 # Gets a value POLITELY for each prompt.
-def ask_for(thing):
+def askFor(thing):
   result = None
   while True:
-    typed(f"Please provide {thing}: ", speed=0.005, ending='')
-    result = input()
+    #typed(f"Please provide {thing}: ", speed=0.005, ending='')
+    result = input("Please provide " + thing + ": ")
     if result.strip() == '':
-      typed('\tERROR: please provide a valid input.', 0.001)
+      print("\tERROR: please provide a valid input.")
     else:
       return result.strip()
 
 # Returns proper pronoun for each case.
 def pronoun(id, subject):
-  if id == 'm':
-    return ['he', 'him', 'his'][subject]
-  elif id == 'f':
-    return ['she', 'her', 'hers'][subject]
+  if id == "m":
+    return ["he", "him", "his"][subject]
+  elif id == "f":
+    return ["she", "her", "her(s)"][subject]
   else:
-    return ['they', 'them', 'theirs'][subject]
+    return ["they", "them", "their(s)"][subject]
 
-# Prompts user to continue running the program.
+# Prompts user to continue running the program by pressing enter.
 def pause():
-  input('[PRESS ENTER TO CONTINUE]')
+  input("[PRESS ENTER]")
 
 #*** INTRODUCTION ***#
-typed('Welcome to an exciting game of...')
-typed('MAD LIBS!!!', speed=0.4)
+print("Welcome to an exciting game of...")
+print("MAD LIBS!!!")
 print()
 
-typed('The rules of this game are simple: provide the words that I ask for \
-and I will generate a very random story for you to read!', 0.00001)
-typed('Are you ready? Let\'s begin!')
-print()
 pause()
+print()
+
+print("The rules of this game are simple: provide the words that I ", end="")
+print("ask for and I will generate a very random story for you to read!")
+print()
+
+print("Are you ready? Let\'s begin!")
+print()
+
+pause()
+print()
 
 #*** INPUT ***#
-print()
-typed("Following this message, provide input according to what the \
-prompt demands.", 0.005)
-print('————————————————————')
+print('Following this message, provide input according to what the ', end='')
+print("prompt demands.")
+print("————————————————————")
 
-name = ask_for('your name')
+name = askFor("your name")
 
+# This loop continues looping until the user provides their gender.
 while True:
-  gender = ask_for('your gender - male [m], female [f], other [nb]')
-  if gender not in ['m', 'f', 'nb']:
-    typed('\tERROR: invalid input, please try again.', 0.001)
+  gender = askFor("your gender - male [m], female [f], other [nb]")
+  if gender not in ["m", "f", "nb"]:
+    print("\tERROR: invalid input, please try again.")
   else:
     break
   
-celeb = ask_for('the name of a famous person')
-month = ask_for('a month')
-day = ask_for('a day')
-place = ask_for('a place')
-action1 = ask_for('an action verb [past tense]')
-action2 = ask_for('an action verb [ending with "-ing"]')
-adjective = ask_for('an adjective')
-emergency = ask_for('an emergency service')
-food = ask_for('a food')
-phone = ask_for('an object')
-ride = ask_for('any moving noun')
-obstacles = ask_for('a noun [plural]')
-animals = ask_for('an animal [plural]')
-event = ask_for('an event (the...)')
-punishment = ask_for('punishment [verb] (had to...)')
+celeb = askFor("the name of a famous person")
+month = askFor("a month")
+day = askFor("a day [number]")
+place = askFor("a place")
+action1 = askFor("an action verb [past tense]")
+action2 = askFor("an action verb [ending with \"-ing\"]")
+adjective = askFor("an adjective")
+emergency = askFor("an emergency service")
+food = askFor("a food")
+phone = askFor("an object")
+ride = askFor("any moving noun")
+obstacles = askFor("a noun [plural]")
+animals = askFor("an animal [plural]")
+event = askFor("an event (the...)")
+punishment = askFor("punishment [verb] (had to...)")
 
 #*** OUTPUT ***#
 print()
-typed('Generating story.........')
-typed('DONE.')
+print("Generating story.........")
+print("DONE.")
 print()
-pause()
 
+pause()
 print()
-typed('\n\n\n\n\nA MORNING OF MISHAPS')
-print('————————————————————')
+
+# Title Heading
+print("\n\n\n\n\nA MORNING OF MISHAPS")
+print("————————————————————\n")
+
+# The comments before each section of print statements are the output.
+
+# "The morning of {month} {day}, little {name} was walking to {place}."
+print("The morning of", month, day + ",", "little", name, end='')
+print(" was walking to", place + ".")
 print()
-typed(f'The morning of {month} {day}, little {name} was walking to {place}.') 
+
+# "It was a nice sunny day and so {he/she} smiled to {him/her}self pleasantly.
+# The air was warm with a nice crisp breeze that lightly wisped {name}'s hair."
+print("It was a nice sunny day and so", pronoun(gender, 0), "smiled", end="")
+print(" to", pronoun(gender, 1) + "self", "pleasantly. The air was ", end="")
+print("warm with a nice crisp breeze that lightly wisped", name + "'s hair.")
 print()
-typed(f'On the way to {place}, {name} saw {celeb} lying on the street. What \
-happened to poor {celeb}? It seems that they may have been {action1} in the \
-middle of the street. Oh dear!')
+
+pause()
 print()
-typed(f'{name} helps them by {action2} and calling {emergency}, \
-before continuing on {pronoun(gender, 2)} way to {place}.')
+
+# "On the way to {place}, {name} saw {celebrity name} lying on the street.
+# What happened to poor {celebrity name}? It seems that they may have been
+# {action verb | past tense} in the middle of the street. Oh dear!"
+print("On the way to", place + ",", name, "saw", celeb, "lying ", end="")
+print("on the street. What happened to poor " + celeb + "? It seems ", end="")
+print("that they may have been", action1, "in the middle of the ", end="")
+print("street. Oh dear!")
 print()
-typed(f'Walking to {place} is quite tiring for {pronoun(gender, 1)}\
-. "What should I eat for breakfast," says {name}, who forgot to eat \
-breakfast this morning. As {pronoun(gender, 0)} walks into a store, \
-{pronoun(gender, 0)} decides on {food}.')
+
+# "{celebrity name} screams for help just as {name} starts walking 
+# towards them."
+print(celeb, "screams for help just as", name, "starts walking towards them.")
 print()
-typed(f'Noticing that {pronoun(gender, 0)} is almost late, it \
-would be smart for {name} to call using {pronoun(gender, 2)} \
-{phone} to hail a ride on a(n) {ride} to {place}.')
+
+# "{name} helps by {action verb | -ing} them and calling the {emergency 
+# service} before continuing on {his/her} way to {place}."
+print(name, "helps by", action2, "them and calling the", emergency, end="")
+print(" before continuing on", pronoun(gender, 2), "way to", place + ".")
 print()
-typed(f'Due to {obstacles} and {animals} on the road, there was a \
-great big traffic jam.')
+
+pause()
 print()
-typed(f'Sadly, because this mode of transportation was slow and due \
-to the congestion, {name} was late to participate in the {event} and \
-thus forced to {punishment} as punishment.')
+
+# "Walking to {place} is quite tiring for {him/her}. 'What should I eat for
+# breakfast,' says {name}, who forgot to eat breakfast this morning. Maybe
+# if {name} stocked up on energy, {he/she} would be able to continue on."
+print("Walking to", place, "is quite tiring for ", end='')
+print(pronoun(gender, 1)+".", "\"What should I eat for breakfast,\" ", end="")
+print("says", name + ",", "who forgot to eat breakfast this morning. ", end="")
+print("Maybe if", name, "stocked up on energy,", pronoun(gender, 0), end="")
+print(" would be able to continue on", pronoun(gender, 2), "journey.")
 print()
-typed(f'What a(n) long {adjective} day it was for {name}.')
+
+# "As {he/she} walks into a store, {he/she} decides on {food}."
+print("As", pronoun(gender, 0), "walks into a store, ", end="")
+print(pronoun(gender, 0), "decides on " + food + ".")
+print()
+
+pause()
+print()
+
+# "On the street, there was a great abundance of {animals}."
+print("On the street, there was a great abundance of", animals + ".")
+print()
+
+# "Noticing that {he/she} is almost late, it would be smart for {name} to call 
+# using {his/her} {object} to hail a ride on a(n) {transportation} to {place}.
+print("Noticing that", pronoun(gender, 0), "is almost late, it ", end="")
+print("would be smart for", name, "to call using", pronoun(gender, 2), end=" ")
+print(phone, "to hail a ride on a(n)", ride, "to", place + ".")
+print()
+
+pause()
+print()
+
+# "The ride was as comfortable as any ride on a(n) {transportation} would be, 
+# but due to {plural noun} and {plural animals} on the road, there was a great
+# big traffic jam."
+print("The ride was as comfortable as any ride on a(n)", ride, end="")
+print(" would be, but ", end="")
+print("due to", obstacles, "and", animals, "on the road, there was a ", end="")
+print("great big traffic jam.")
+print()
+
+# "Sadly, because this mode of transportation was slow and due to the to the 
+# congestion, {name} was late to participate in the {event} and thus was forced
+# to {punishment} as punishment."
+print("Sadly, because this mode of transportation was slow and due ", end="")
+print("to the congestion,", name, "was late to participate in the ", end="")
+print(event, "and thus had to", punishment, "as punishment.")
+print()
+
+# "What a long {adjective} day it was for {name}."
+print("What a long", adjective, "day it was for", name + ".\n")
+
+print("The end.")
